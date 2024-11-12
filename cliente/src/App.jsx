b42776367258acc0b4b07f01f7f5df8be4250416
Inti Josué Cotelo <cotelointi@outlook.com>
2024-11-11 21:12:07 -0300

    Creación de cliente usando React

---
+++ cliente/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <ul id="messages"></ul>
+      <form id="form" action="">
+        <input id="input" autocomplete="off" /><button>Send</button>
+      </form>
+    </>
+  )
+}
+
+export default App
